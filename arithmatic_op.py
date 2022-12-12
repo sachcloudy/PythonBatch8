32e11e4b367080a883be9ccf46faecda82fd96cd
Sachcloudy <tejasmali4@gmail.com>
2022-12-12 15:02:22 +0530

    added code to MAZ-001

--- arithmatic_op.py
+++ arithmatic_op.py
@@ -1,6 +1,10 @@
-def addition():
+def addition(a,b):
     return a + b
+
+def subtraction(a,b):
+    return a - b
 
 a = 10
 b = 15
 print (f"Addition of {a} and {b} is {addition (a,b)}")
+print (f"subtraction of {a} and {b} is {subtraction (a ,b)}")
